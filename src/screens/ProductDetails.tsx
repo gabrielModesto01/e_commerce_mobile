9e259312c93c7a8b32594c4b68570bfe5fa6eafa
Gabriel Modesto <gmodesto2012@gmail.com>
2023-08-14 18:49:22 -0300

    Inicio da Implantação do DetailsProduct
    Tytle e Img

--- src/screens/ProductDetails.tsx
+++ src/screens/ProductDetails.tsx
@@ -1,10 +1,12 @@
 import { View } from 'react-native'
 import DetaislsBar from '../components/DetailsBar'
+import DetailsProduct from '../components/DetailsProduct'
 
 export default function ProductsDetaisls() {
     return (
         <View className='h-full flex 1 bg-background_Details px-[15px] pt-[32px]'>
             <DetaislsBar />
+            <DetailsProduct />
         </View>
     )
 }
